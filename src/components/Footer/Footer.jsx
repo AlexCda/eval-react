02b78d26d4acfaf06ed510478455b3af0071057c
AlexCda <alexandre.teixeira@ccicampus.fr>
2025-07-30 08:50:23 +0200

    style sur le footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import classes from './Footer.module.css'
 const Footer = () => {
     return (
-        <footer>
+        <footer className={classes.footer}>
             <p>© 2025 Moviz. Tous droits réservés</p>
         </footer>
     )
